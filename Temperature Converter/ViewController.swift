b22d338b7ff9d7ed5e7fc6b242e529d26e4a2cae
Ali Mazeh <Ali@alimazeh7.local>
2017-01-08 17:12:44 -0500

    Added IBOutlet properties/methods

--- Temperature Converter/ViewController.swift
+++ Temperature Converter/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textBox: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,67 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func equalButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func zeroTapped(_ sender: Any) {
+    }
+    
+    @IBAction func oneTapped(_ sender: Any) {
+    }
 
+    @IBAction func twoTapped(_ sender: Any) {
+    }
+    
+    @IBAction func threeTapped(_ sender: Any) {
+    }
+    
+    @IBAction func fourTapped(_ sender: Any) {
+    }
+    
+    @IBAction func fiveTapped(_ sender: Any) {
+    }
+    
+    @IBAction func sixTapped(_ sender: Any) {
+    }
+    
+    @IBAction func sevenTapped(_ sender: Any) {
+    }
+    
+    @IBAction func eightTapped(_ sender: Any) {
+    }
+    
+    @IBAction func nineTapped(_ sender: Any) {
+    }
+    
+    @IBAction func negButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func decimalButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func percentButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func divButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func multButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func subButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
 }
 
