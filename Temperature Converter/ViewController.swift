661c2710183c9d759c71e74683ecc17c0f2e3fd2
Ali Mazeh <Ali@alimazeh7.local>
2017-01-08 21:10:28 -0500

    Added more functionality to number buttons

--- Temperature Converter/ViewController.swift
+++ Temperature Converter/ViewController.swift
@@ -7,10 +7,41 @@
 
 import UIKit
 
+// extension to allow use of multiple string subscripts
+extension String {
+    
+    var length: Int {
+        return self.characters.count
+    }
+    
+    subscript (i: Int) -> String {
+        return self[Range(i ..< i + 1)]
+    }
+    
+    func substring(from: Int) -> String {
+        return self[Range(min(from, length) ..< length)]
+    }
+    
+    func substring(to: Int) -> String {
+        return self[Range(0 ..< max(0, to))]
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)), upper: min(length, max(0, r.upperBound))))
+        
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return self[Range(start ..< end)]
+    }
+}
+
 class ViewController: UIViewController {
+    
 
     @IBOutlet weak var textBox: UITextField!
     var textBoxValue = ""
+    var zeroCheck = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +61,15 @@ class ViewController: UIViewController {
     }
     
     @IBAction func zeroTapped(_ sender: Any) {
+        // get current value in text box
+        textBox.text = textBoxValue;
+        // check to not allow multiple leading zeros
+        if textBoxValue != "0" {
+            // append the number one to the end of it
+            textBoxValue += "0"
+            // set text box to new value
+            textBox.text = textBoxValue
+        }
         
     }
     
@@ -41,48 +81,96 @@ class ViewController: UIViewController {
 
     @IBAction func twoTapped(_ sender: Any) {
         textBox.text = textBoxValue
+        zeroCheck = textBoxValue[0]
+        
+        if zeroCheck == "0" {
+            textBoxValue.removeAll()
+        }
+        
         textBoxValue += "2"
         textBox.text = textBoxValue
     }
     
     @IBAction func threeTapped(_ sender: Any) {
         textBox.text = textBoxValue
+        zeroCheck = textBoxValue[0]
+        
+        if zeroCheck == "0" {
+            textBoxValue.removeAll()
+        }
+        
         textBoxValue += "3"
         textBox.text = textBoxValue
     }
     
     @IBAction func fourTapped(_ sender: Any) {
         textBox.text = textBoxValue
+        zeroCheck = textBoxValue[0]
+        
+        if zeroCheck == "0" {
+            textBoxValue.removeAll()
+        }
+        
         textBoxValue += "4"
         textBox.text = textBoxValue
     }
     
     @IBAction func fiveTapped(_ sender: Any) {
         textBox.text = textBoxValue
+        zeroCheck = textBoxValue[0]
+        
+        if zeroCheck == "0" {
+            textBoxValue.removeAll()
+        }
+        
         textBoxValue += "5"
         textBox.text = textBoxValue
     }
     
     @IBAction func sixTapped(_ sender: Any) {
         textBox.text = textBoxValue
+        zeroCheck = textBoxValue[0]
+        
+        if zeroCheck == "0" {
+            textBoxValue.removeAll()
+        }
+        
         textBoxValue += "6"
         textBox.text = textBoxValue
     }
     
     @IBAction func sevenTapped(_ sender: Any) {
         textBox.text = textBoxValue
+        zeroCheck = textBoxValue[0]
+        
+        if zeroCheck == "0" {
+            textBoxValue.removeAll()
+        }
+        
         textBoxValue += "7"
         textBox.text = textBoxValue
     }
     
     @IBAction func eightTapped(_ sender: Any) {
         textBox.text = textBoxValue
+        zeroCheck = textBoxValue[0]
+        
+        if zeroCheck == "0" {
+            textBoxValue.removeAll()
+        }
+        
         textBoxValue += "8"
         textBox.text = textBoxValue
     }
     
     @IBAction func nineTapped(_ sender: Any) {
         textBox.text = textBoxValue
+        zeroCheck = textBoxValue[0]
+        
+        if zeroCheck == "0" {
+            textBoxValue.removeAll()
+        }
+        
         textBoxValue += "9"
         textBox.text = textBoxValue
     }
